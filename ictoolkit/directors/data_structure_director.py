9455889f236eb69299b756095f8eeadaa6ab2a43
IncognitoCoding <78840063+IncognitoCoding@users.noreply.github.com>
2021-09-25 23:18:04 -0500

    Added string_grouper function

--- ictoolkit/directors/data_structure_director.py
+++ ictoolkit/directors/data_structure_director.py
@@ -7,12 +7,13 @@ This module is designed to offer data structure functions. These data structures
 import os
 import logging
 import traceback
+from itertools import groupby
 
 __author__ = 'IncognitoCoding'
 __copyright__ = 'Copyright 2021, data_structure_director'
 __credits__ = ['IncognitoCoding']
 __license__ = 'GPL'
-__version__ = '1.3'
+__version__ = '1.4'
 __maintainer__ = 'IncognitoCoding'
 __status__ = 'Development'
 
@@ -360,3 +361,205 @@ def get_list_duplicates(duplicates, match_index=None, grouped=False):
                 return duplicate_list_dictionary
         else:
             return None
+
+
+def string_grouper(list_of_strings, grouping_value, grouping_option):   
+    """
+    String grouper will group a list of strings using three different options. Each option provides a different type of results, but covers any type of desired grouping.
+
+    Args:
+        list_of_strings (list): A grouping of strings that need to get grouped.
+        grouping_value (str or int): A group value based on a character or positions.\\
+            \- Group Option 1 Example: grouping_value = -. If the string is "Testing-1", the grouping values would match on "Testing".\\
+            \- Group Option 2 Example: grouping_value = 2. Result: If the string is "Testing-1", the grouping values would match on "Te".\\
+            \- Group Option 3 Example: No value is required. Send None or a random character as the parameter value.\\
+        grouping_option (int): A grouping option.
+            \- Option 1:
+                \- Similar Separator\\
+                    \- Using this option will use the "grouping_value" to group all like entries in the list together.\\
+            \- Option 2:
+                \- Character Position Number\\
+                    \- Using this option will group all like entries in the list together based on the number of character positions entered.\\
+            \- Option 3:
+                \- Common String\\
+                    \- Using this option will group all common strings in the list together based on the last matching character.\\
+                        \- Note: The group_identifier may not be the same number of characters based on the importing list.\\
+
+    Raises:
+        ValueError: The value sent for the grouping is not a string.
+        ValueError: The value sent for the grouping is not a number.
+    
+    Returns:
+        list: A list of grouped strings.
+            \- Return Output: [{'group_identifier': 'BA', 'grouping': ['BA-IDF1', 'BA-IDF2', 'BA-IDF3', 'BA-MDF']}, {'group_identifier': 'CB', 'grouping': ['CB-IDF0', 'CB-IDF1', 'CB-MDF']}, 
+                                         \{'group_identifier': 'CE', 'grouping': ['CE-BUSLABOFF', 'CE-IDF-Band']}, {'group_identifier': 'CI', 'grouping': ['CI-MDF1']},\ 
+                                         \{'group_identifier': 'DH', 'grouping': ['DH-IDF1', 'DH-IDF2', 'DH-MDF']}, {'group_identifier': 'GH', 'grouping': ['GH-IDF0-RM113', 'GH-IDF1-RM161', 'GH-MDF-RM113']}]
+    """  
+    logger = logging.getLogger(__name__)
+
+    # Validates the sent strings are in a list.
+    if isinstance(list_of_strings, list):
+        # Make sure that the list is greater than or equal to 2.
+        if len(list_of_strings) >= 2:
+            # Holds grouped values from the list.
+            grouping = []
+            # Sort the list.
+            # This is essential for grouping.
+            list_of_strings.sort()
+
+            # Groups based on the users group option.
+            if grouping_option == 1:
+                # This section groups based on a character. If the string is "Testing-1" and the matching character was -, the grouping values would match on "Testing".
+                # Checks that the grouping_value is a string.
+                if isinstance(grouping_value, str):
+                    # Loops through a list of strings and splits based on the split number. The split number will create a grouping, and provide the split output for each grouping.
+                    for j, i in groupby(list_of_strings, lambda a_string: a_string.split(grouping_value)[0]):
+                        # Appends the grouping from groupby to the list in in dictionary format.
+                        grouping.append({'group_identifier': j, 'grouping': list(i)})
+                else:
+                    error_message = (
+                        f'The grouping_value sent for the grouping is not a string.\n' +
+                        (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n') +
+                        'Expected Result:\n'
+                        '  - string\n\n'
+                        'Returned Result:\n'
+                        f'  - grouping_value = {grouping_value}\n\n'
+                        f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n' +
+                        (('-' * 150) + '\n') * 2 
+                    )
+                    logger.error(error_message)
+                    raise ValueError(error_message)
+            elif grouping_option == 2:
+                # This section groups based on a character number. If the string is "Testing" and the number was 3, the grouping values would match on "Tes".
+                # Checks that the grouping_value is a number.
+                if isinstance(grouping_value, int):
+                    # Loops through a list of strings and splits based on the split number. The split number will create a grouping, and provide the split output for each grouping.
+                    for j, i in groupby(list_of_strings, lambda a_string: [a_string[index : index + grouping_value] for index in range(0, len(a_string), grouping_value)][0]):
+                        # Appends the grouping from groupby to the list in in dictionary format.
+                        grouping.append({'group_identifier': j, 'grouping': list(i)})
+                else:
+                    error_message = (
+                        f'The grouping_value sent for the grouping is not a int.\n' +
+                        (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n') +
+                        'Expected Result:\n'
+                        '  - int\n\n'
+                        'Returned Result:\n'
+                        f'  - grouping_value = {grouping_value}\n\n'
+                        f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n' +
+                        (('-' * 150) + '\n') * 2 
+                    )
+                    logger.error(error_message)
+                    raise ValueError(error_message)
+            elif grouping_option == 3:
+                # This compare can have some complex checks because it has to check previous entries and make choices based on previous and current groupings.
+                # The section of code is split by two different loops. The primary loop goes through each string. The sub-string loops and checks character by character between the previous string and current string.
+                # All processing is done with the mindset that the current entry is always compared with the previous entry. The compare is based on alphabetical order.
+                # Loops through all raw string imports to compare.
+                for raw_string_loop_tracker, string in enumerate(list_of_strings):
+                    # Checks if the length of the raw string is equal to the raw_string_loop_tracker + 1. This allows a clean exit without the list going out of index.
+                    if len(list_of_strings) != raw_string_loop_tracker + 1:
+                        logger.debug(f'Comparing \"{string}\" with \"{list_of_strings[raw_string_loop_tracker + 1]}\"')
+                        # Loops through each character in the main string entry.
+                        for character_loop_tracker, character in enumerate(string):
+                            logger.debug(f'Checking character position {character_loop_tracker}')
+                            # Checks if the character from the main entry matches the character in the next raw string in the list.
+                            # + 1 so the first string entry will compare with this starting entry.
+                            if character == list_of_strings[raw_string_loop_tracker + 1][character_loop_tracker]:
+                                logger.debug(f'Match at character position {character_loop_tracker}')
+                            # No match
+                            elif character != list_of_strings[raw_string_loop_tracker + 1][character_loop_tracker]:
+                                logger.debug(f'No Match at character position {character_loop_tracker}')
+                                # Gets matching characters. Character get starts at 1, so need to -1.
+                                match_characters = string[0:character_loop_tracker]
+                                logger.debug(f'Matched characters = {match_characters}')
+                                if match_characters:
+                                    # Checks if no groupings have been added.
+                                    if not grouping:
+                                        # Adds the initial grouping.
+                                        grouping.append({'group_identifier': match_characters, 'grouping': [string, list_of_strings[raw_string_loop_tracker + 1]]})
+                                    else:
+                                        # Gets the previous grouping identifier.
+                                        previous_group_identifier = grouping[-1].get('group_identifier')
+                                        # Checks if the previous grouping identifier is the same, so the string can be joined into the same group entry.
+                                        if match_characters == previous_group_identifier:
+                                            logger.debug(f'Previous grouping matches. {previous_group_identifier} = {match_characters}')
+                                            # Inserts the compared string to the previous grouping because it has the same matching group identifier.
+                                            grouping[-1].get('grouping').insert(len(grouping) + 1, list_of_strings[raw_string_loop_tracker + 1])
+                                        else:
+                                            # Checks if a single string entry. This is required because a single string entry will have the same group_identifier name as the string until the next entry is matched.
+                                            if len(grouping[-1].get('grouping')) == 1:
+                                                logger.debug(f'Previous grouping is a single entry and has matching characters. \"{match_characters}\" in \"{previous_group_identifier}\"')
+                                                # Checks if the current match_characters are in the previous_group_identifiers name.
+                                                # Note: The previous_group_identifiers name will be the full name of the string, so the match has to be the other way for detection.
+                                                if match_characters in previous_group_identifier:
+                                                    logger.debug('Merging previous entry with the current entry')
+                                                    # Remove previous entry and insert the grouping from the previous entry with the current string group.
+                                                    #
+                                                    #
+                                                    # Gets the previous grouping string. Removes the list brackets and quotes.
+                                                    previous_group_string = str(grouping[-1].get('grouping')).strip('][').strip("'")
+                                                    
+                                                    # Removes the previous group entry because it will not be merged with the current string group.
+                                                    grouping.remove(grouping[-1])
+                                                    # Adds the new grouping to the list with the previous group added as well.
+                                                    grouping.append({'group_identifier': match_characters, 'grouping': [previous_group_string, list_of_strings[raw_string_loop_tracker + 1]]})
+                                                else:
+                                                    logger.debug(f'Previous grouping do not match. {previous_group_identifier} != {match_characters}')
+                                                    # Adds the new grouping to the list.
+                                                    grouping.append({'group_identifier': match_characters, 'grouping': [list_of_strings[raw_string_loop_tracker + 1]]})
+                                            # Compares the previous group to the match to make sure the groupings are the same. The previous_grouping_identifier could contain more characters than the current match, so this flow is required.
+                                            elif previous_group_identifier in match_characters:
+                                                logger.debug(f'Previous grouping has multiple strings grouped and the group_identifier has the same characters as the match group. \"{match_characters}\" in \"{previous_group_identifier}\"')
+                                                # Inserts the compared string to the previous grouping because it has the same matching group identifier.
+                                                grouping[-1].get('grouping').insert(len(grouping) + 1, list_of_strings[raw_string_loop_tracker + 1])
+                                            else:
+                                                logger.debug(f'Previous grouping do not match. {previous_group_identifier} != {match_characters}')
+                                                # No match occurred with the previous entry, which means this entry is a completely new entry, so the "group_identifier" will be the name of the string. This will adjust if the next string has a match.
+                                                # Note: If no match is made the group_identifier will always be the name of the string because it had nothing to compare itself against.
+                                                # Adds the new grouping to the list.
+                                                grouping.append({'group_identifier': list_of_strings[raw_string_loop_tracker + 1], 'grouping': [list_of_strings[raw_string_loop_tracker + 1]]})
+                                else:
+                                    logger.debug('No Matching Characters Found')
+
+                                    # Checks if entries have been added to the list. No entries means the starting entry needs added.
+                                    if not grouping:
+                                        # Adds the starting entry and the next entry because neither of these entries matched on startup.
+                                        grouping.append({'group_identifier': string, 'grouping': [string]})
+                                        grouping.append({'group_identifier': list_of_strings[raw_string_loop_tracker + 1], 'grouping': [list_of_strings[raw_string_loop_tracker + 1]]})
+                                    else:
+                                        # Note: If no match is made the group_identifier will always be the name of the string because it had nothing to compare itself against.
+                                        # Adds the new grouping to the list.
+                                        grouping.append({'group_identifier': list_of_strings[raw_string_loop_tracker + 1], 'grouping': [list_of_strings[raw_string_loop_tracker + 1]]})
+
+                                # Breaks the lop because no additional characters match.
+                                break
+                            
+                            # Checks if the main string is shorter than the main string. This means no match occurred, so the group_identifier for this entry is main entry.
+                            if character_loop_tracker == len(string) - 1:
+                                logger.debug('Compare string is longer than the main string')
+                                grouping.append({'group_identifier': string, 'grouping': [string]})
+                                grouping.append({'group_identifier': list_of_strings[raw_string_loop_tracker + 1], 'grouping': [list_of_strings[raw_string_loop_tracker + 1]]})
+
+            # Returns the list of dictionaries.
+            # Return Example: grouping = [{'group_identifier': 'JJ-MDF-9200-1_2', 'grouping': ['JJ-MDF-9200-1_2']},
+            #                             {'group_identifier': 'KV-IDF', 'grouping': ['KV-IDF1-9200-1_2', 'KV-IDF2-9200-1_2']},
+            #                             {'group_identifier': 'KV-MDF', 'grouping': ['KV-MDF-9200-1_2', 'KV-MDF1-9200-1_2']},
+            #                             {'group_identifier': 'KZV-MDF1-9200-1_2', 'grouping': ['KZV-MDF1-9200-1_2']},
+            #                             {'group_identifier': 'TI-IDF', 'grouping': ['TI-IDF1-9200-1_2', 'TI-IDF2-9200-1_2']}]
+            return grouping
+        else:
+            return None
+    else:
+        list_of_strings = type(list_of_strings)
+        error_message = (
+            'The sent strings is not in list format.\n' +
+            (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n') +
+            'Expected Result:\n'
+            f'  - list_of_strings = list\n\n'
+            'Returned Result:\n'
+            f'  - list_of_strings = {list_of_strings}\n\n'
+            f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n' +
+            (('-' * 150) + '\n') * 2 
+        )
+        logger.error(error_message)
+        raise ValueError(error_message)
