f3056ce6dfeafb3ba9979f66a664b0b522bee6f2
IncognitoCoding <78840063+IncognitoCoding@users.noreply.github.com>
2021-09-28 09:56:14 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.0.7.4' 
+VERSION = '0.0.7.5' 
 DESCRIPTION = 'ictoolkit is designed to be the swiss army knife of programming methods.'
 LONG_DESCRIPTION = 'Each method is broken down into a specific type of use case. The methods are currently broad and cover several types of areas.'
 
