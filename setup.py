6f9a296a2aaa94086975f64dc18751c47f8ec8d2
IncognitoCoding <78840063+IncognitoCoding@users.noreply.github.com>
2021-12-21 14:38:35 -0600

    Update setup.py
    Added new helpers package directory.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.0.10.4'
+VERSION = '0.0.10.5'
 DESCRIPTION = 'ictoolkit is designed to be the swiss army knife of programming methods.'
 LONG_DESCRIPTION = 'Each method is broken down into a specific type of use case. The methods are currently broad and cover several types of areas.'
 
@@ -11,7 +11,10 @@ setup(
     author="ictoolkit",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    packages=['ictoolkit/directors'],
+    packages=[
+        'ictoolkit/directors',
+        'ictoolkit/helpers'
+    ],
     url='https://github.com/IncognitoCoding/ictoolkit.git',
     license='GPL',
     install_requires=[
