cebb9f1810327457f91c0b410e41a822b30d728e
IncognitoCoding <78840063+IncognitoCoding@users.noreply.github.com>
2021-12-21 15:28:23 -0600

    Added multiple type match option

--- ictoolkit/directors/validation_director.py
+++ ictoolkit/directors/validation_director.py
@@ -4,18 +4,18 @@ This module is designed for validation checks. All modules will not return any d
 # Built-in/Generic Imports
 import traceback
 import logging
-from typing import Any, Optional
+from typing import Any, Optional, Union
 
 __author__ = 'IncognitoCoding'
 __copyright__ = 'Copyright 2021, validation_director'
 __credits__ = ['IncognitoCoding']
 __license__ = 'GPL'
-__version__ = '1.1'
+__version__ = '1.2'
 __maintainer__ = 'IncognitoCoding'
 __status__ = 'Development'
 
 
-def value_type_validation(value: any, required_type: type, caller_module: Optional[str] = None, caller_function: Optional[str] = None, caller_line: Optional[int] = None) -> None:
+def value_type_validation(value: any, required_type: Union[type, list], caller_module: Optional[str] = None, caller_function: Optional[str] = None, caller_line: Optional[int] = None) -> None:
     """
     A simple type validation validation check.
 
@@ -23,7 +23,7 @@ def value_type_validation(value: any, required_type: type, caller_module: Option
 
     Args:
         value (any): Any value needing its type validated.
-        required_type (type): The required type the value should match.
+        required_type (type or list of types): The required type the value should match. Can be a single type or list of types.
         caller_module (str, optional): The name of the caller module. Use \'__name__\'. Defaults to None.
         caller_function (str, optional): The calling function name. Defaults to None.
         caller_line (int, optional): The calling function line. Defaults to None.
@@ -44,106 +44,148 @@ def value_type_validation(value: any, required_type: type, caller_module: Option
         f'  - required_type (type):\n        - {required_type}'
     )
 
-    # Verify the value matches the required type.
-    if not isinstance(value, required_type):
+    # Verifies a value is sent.
+    if value:
+        # Verifies a type or list is sent.
         if (
-            caller_module
-            and caller_function
-            and caller_line
+            isinstance(required_type, list)
+            or isinstance(required_type, type)
         ):
+            if isinstance(required_type, list):
+                for value_type in required_type:
+                    if isinstance(value, value_type):
+                        matching_type_flag = True
+                        break
+                    else:
+                        matching_type_flag = False
+            else:
+                # Verifies the returning YAML value.
+                if not isinstance(value, required_type):
+                    matching_type_flag = False
+                else:
+                    matching_type_flag = True
+            # Checks for no match.
+            if matching_type_flag is False:
+                if (
+                    caller_module
+                    and caller_function
+                    and caller_line
+                ):
+                    error_message = (
+                        f'The value \'{value}\' is not in {required_type} format.\n'
+                        + (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n')
+                        + 'Caller Module:\n'
+                        f'  - {caller_module}\n\n'
+                        'Caller Function:\n'
+                        f'  - {caller_function}\n\n'
+                        'Caller Line:\n'
+                        f'  - {caller_line}\n\n'
+                        'Expected Result:\n'
+                        f'  - Type = {required_type}\n\n'
+                        'Returned Result:\n'
+                        f'  - Type = {type(value)}\n\n'
+                        f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n'
+                        + (('-' * 150) + '\n') * 2
+                    )
+                    logger.error(error_message)
+                    raise TypeError(error_message)
+                elif (
+                    caller_module
+                    and not caller_function
+                    and not caller_line
+                ):
+                    error_message = (
+                        f'The value \'{value}\' is not in {required_type} format.\n'
+                        + (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n')
+                        + 'Caller Module:\n'
+                        f'  - {caller_module}\n\n'
+                        'Expected Result:\n'
+                        f'  - Type = {required_type}\n\n'
+                        'Returned Result:\n'
+                        f'  - Type = {type(value)}\n\n'
+                        f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n'
+                        + (('-' * 150) + '\n') * 2
+                    )
+                    logger.error(error_message)
+                    raise TypeError(error_message)
+                elif (
+                    caller_module
+                    and not caller_function
+                    and caller_line
+                ):
+                    error_message = (
+                        f'The value \'{value}\' is not in {required_type} format.\n'
+                        + (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n')
+                        + 'Caller Module:\n'
+                        f'  - {caller_module}\n\n'
+                        'Caller Line:\n'
+                        f'  - {caller_line}\n\n'
+                        'Expected Result:\n'
+                        f'  - Type = {required_type}\n\n'
+                        'Returned Result:\n'
+                        f'  - Type = {type(value)}\n\n'
+                        f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n'
+                        + (('-' * 150) + '\n') * 2
+                    )
+                    logger.error(error_message)
+                    raise TypeError(error_message)
+                elif (
+                    not caller_module
+                    and caller_line
+                    and caller_function
+                ):
+                    error_message = (
+                        f'The value \'{value}\' is not in {required_type} format.\n'
+                        + (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n')
+                        + 'Caller Function:\n'
+                        f'  - {caller_function}\n\n'
+                        'Caller Line:\n'
+                        f'  - {caller_line}\n\n'
+                        'Expected Result:\n'
+                        f'  - Type = {required_type}\n\n'
+                        'Returned Result:\n'
+                        f'  - Type = {type(value)}\n\n'
+                        f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n'
+                        + (('-' * 150) + '\n') * 2
+                    )
+                    logger.error(error_message)
+                    raise TypeError(error_message)
+                elif (
+                    not caller_module
+                    and not caller_function
+                    and not caller_line
+                ):
+                    error_message = (
+                        f'The value \'{value}\' is not in {required_type} format.\n'
+                        + (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n')
+                        + 'Expected Result:\n'
+                        f'  - Type = {required_type}\n\n'
+                        'Returned Result:\n'
+                        f'  - Type = {type(value)}\n\n'
+                        f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n'
+                        + (('-' * 150) + '\n') * 2
+                    )
+                    logger.error(error_message)
+                    raise TypeError(error_message)
+        else:
             error_message = (
-                f'The value \'{value}\' is not in {required_type} format.\n'
+                f'No type or list of types has been entered for type validation.\n\n'
                 + (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n')
-                + 'Caller Module:\n'
-                f'  - {caller_module}\n\n'
-                'Caller Function:\n'
-                f'  - {caller_function}\n\n'
-                'Caller Line:\n'
-                f'  - {caller_line}\n\n'
-                'Expected Result:\n'
-                f'  - Type = {required_type}\n\n'
-                'Returned Result:\n'
-                f'  - Type = {type(value)}\n\n'
+                + 'Suggested Resolution:\n'
+                f'  - Please check the calling function.\n\n'
                 f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n'
                 + (('-' * 150) + '\n') * 2
             )
             logger.error(error_message)
-            raise TypeError(error_message)
-        elif (
-            caller_module
-            and not caller_function
-            and not caller_line
-        ):
-            error_message = (
-                f'The value \'{value}\' is not in {required_type} format.\n'
-                + (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n')
-                + 'Caller Module:\n'
-                f'  - {caller_module}\n\n'
-                'Expected Result:\n'
-                f'  - Type = {required_type}\n\n'
-                'Returned Result:\n'
-                f'  - Type = {type(value)}\n\n'
-                f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n'
-                + (('-' * 150) + '\n') * 2
-            )
-            logger.error(error_message)
-            raise TypeError(error_message)
-        elif (
-            caller_module
-            and not caller_function
-            and caller_line
-        ):
-            error_message = (
-                f'The value \'{value}\' is not in {required_type} format.\n'
-                + (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n')
-                + 'Caller Module:\n'
-                f'  - {caller_module}\n\n'
-                'Caller Line:\n'
-                f'  - {caller_line}\n\n'
-                'Expected Result:\n'
-                f'  - Type = {required_type}\n\n'
-                'Returned Result:\n'
-                f'  - Type = {type(value)}\n\n'
-                f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n'
-                + (('-' * 150) + '\n') * 2
-            )
-            logger.error(error_message)
-            raise TypeError(error_message)
-        elif (
-            not caller_module
-            and caller_line
-            and caller_function
-        ):
-            error_message = (
-                f'The value \'{value}\' is not in {required_type} format.\n'
-                + (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n')
-                + 'Caller Function:\n'
-                f'  - {caller_function}\n\n'
-                'Caller Line:\n'
-                f'  - {caller_line}\n\n'
-                'Expected Result:\n'
-                f'  - Type = {required_type}\n\n'
-                'Returned Result:\n'
-                f'  - Type = {type(value)}\n\n'
-                f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n'
-                + (('-' * 150) + '\n') * 2
-            )
-            logger.error(error_message)
-            raise TypeError(error_message)
-        elif (
-            not caller_module
-            and not caller_function
-            and not caller_line
-        ):
-            error_message = (
-                f'The value \'{value}\' is not in {required_type} format.\n'
-                + (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n')
-                + 'Expected Result:\n'
-                f'  - Type = {required_type}\n\n'
-                'Returned Result:\n'
-                f'  - Type = {type(value)}\n\n'
-                f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n'
-                + (('-' * 150) + '\n') * 2
-            )
-            logger.error(error_message)
-            raise TypeError(error_message)
+            raise ValueError(error_message)
+    else:
+        error_message = (
+            f'No value has been entered for type validation.\n\n'
+            + (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n')
+            + 'Suggested Resolution:\n'
+            f'  - Please check the calling function.\n\n'
+            f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n'
+            + (('-' * 150) + '\n') * 2
+        )
+        logger.error(error_message)
+        raise ValueError(error_message)
