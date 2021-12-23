fe013c0c90ff9a6bf7d52e801730dbd188c74db8
IncognitoCoding <78840063+IncognitoCoding@users.noreply.github.com>
2021-12-23 12:48:23 -0600

    thread_director error output update.

--- ictoolkit/directors/thread_director.py
+++ ictoolkit/directors/thread_director.py
@@ -10,27 +10,29 @@ import sys
 import queue
 import time
 import traceback
-from threading import Thread
-from threading import Event
 import logging
+
+# Own module
+from ictoolkit.directors.error_director import error_formatter
+from ictoolkit.helpers.py_helper import get_function_name, get_line_number
 
 __author__ = 'IncognitoCoding'
 __copyright__ = 'Copyright 2021, thread_director'
 __credits__ = ['IncognitoCoding']
 __license__ = 'GPL'
-__version__ = '1.3'
+__version__ = '1.5'
 __maintainer__ = 'IncognitoCoding'
 __status__ = 'Development'
 
 
 def start_function_thread(passing_program_function, program_function_name, infinite_loop_option):
     """
-    This function is used to start any other function inside it's own thread. This is ideal if you need to have part of the program sleep and another part of the program always 
+    This function is used to start any other function inside it's own thread. This is ideal if you need to have part of the program sleep and another part of the program always
     active (ex: Web Interface = Always Active & Log Checking = 10 Minute Sleep)
 
-    Thread exception capturing offers a challenge because the initialized child thread is in its dedicated context with its dedicated stack. When an exception is thrown in, the 
-    child thread can potentially never report to the parent function. The only time the messages can be present is during the initial call to the child thread. A message bucket 
-    is used to hold any potential exception messages, and a 2 minutes sleep is set to give time for the thread to either start or fail. If neither occurs after 1 minute, the thread 
+    Thread exception capturing offers a challenge because the initialized child thread is in its dedicated context with its dedicated stack. When an exception is thrown in, the
+    child thread can potentially never report to the parent function. The only time the messages can be present is during the initial call to the child thread. A message bucket
+    is used to hold any potential exception messages, and a 2 minutes sleep is set to give time for the thread to either start or fail. If neither occurs after 1 minute, the thread
     will end and throw a value error.
 
     Requires calling program to use "from functools import partial" when calling.
@@ -43,12 +45,12 @@ def start_function_thread(passing_program_function, program_function_name, infin
         infinite_loop_option (bool): Enabled infinite loop.
 
     Raises:
-        ValueError: A failure occurred while staring the function thread.
-        ValueError: The thread ({program_function_name}) timeout has reached its threshold of 1 minute.
+        Exception: A failure occurred while staring the function thread.
+        Exception: The thread ({program_function_name}) timeout has reached its threshold of 1 minute.
     """
     logger = logging.getLogger(__name__)
     logger.debug(f'=' * 20 + traceback.extract_stack(None, 2)[1][2] + '=' * 20)
-    # Custom flowchart tracking. This is ideal for large projects that move a lot. 
+    # Custom flowchart tracking. This is ideal for large projects that move a lot.
     # For any third-party modules, set the flow before making the function call.
     logger_flowchart = logging.getLogger('flowchart')
     logger_flowchart.debug(f'Flowchart --> Function: {traceback.extract_stack(None, 2)[1][2]}')
@@ -65,45 +67,46 @@ def start_function_thread(passing_program_function, program_function_name, infin
             self.name = program_function_name
             self.daemon = True
             self.bucket = bucket
-        # Runs the object as self and calls the function.
+
         def run(self):
+            """Runs the object as self and calls the function."""
             # Stores the exception, if raised by the calling function.
-            self.exception = None  
-            
-            try: 
+            self.exception = None
+
+            try:
                 # Checks if the thread needs to loop.
                 if infinite_loop_option:
-                    # Infiniate Loop.
+                    # Infinite Loop.
                     while True:
                         # Starts the function in a loop.
-                        passing_program_function() 
+                        passing_program_function()
                         # Sleeps 1 seconds to keep system resources from spiking when called without a sleep inside the calling entry.
                         time.sleep(1)
                 else:
                     # Starts the function once.
                     passing_program_function()
-            #Returns the calling functions error message if an error occurs.
+            # Returns the calling functions error message if an error occurs.
             except Exception as err:
                 # Sets the exception error message
-                #self.exception = err
+                # self.exception = err
                 self.bucket.put(sys.exc_info())
-       
+
     # Creates a message queue to hold potential exception messages.
     bucket = queue.Queue()
     # Calls class to start the thread.
     thread_obj = start_function_thread(bucket)
     thread_obj.start()
-    
+
     # Timeout gives enough time for the thread to timeout.
     # This time delay can delay potential incoming streams from a subprocess.
     # If the initial startup is critical, it may be worth creating a small script to delay the start of the subprocess call.
     time.sleep(2)
-    
+
     # Sets max timeout at 1 minute if the thread does not start or an exception is not thrown.
-    timeout = time.time() + 60*1
-    
+    timeout = time.time() + 60 * 1
+
     while True:
-        
+
         try:
             # Gets the bucket values
             exc = bucket.get(block=False)
@@ -113,26 +116,22 @@ def start_function_thread(passing_program_function, program_function_name, infin
             # Sets the bucket values from the exceptions
             exc_type, exc_obj, exc_trace = exc
 
-            error_message = (
-                'A failure occurred while staring the function thread.\n\n' +
-                (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n') +
-                f'{exc_obj}\n\n'
-                f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n' +
-                (('-' * 150) + '\n') * 2 
-            )   
-            raise ValueError(error_message)
-          
+            # Passes the calling functions error output as the original error.
+            error_args = {
+                'main_message': 'A failure occurred while staring the function thread.',
+                'error_type': Exception,
+                'original_error': exc_obj,
+            }
+            error_formatter(error_args, __name__, get_line_number())
+
         # Loop breaks if the thread is alive or timeout reached.
-        if thread_obj.is_alive() == True:
+        if thread_obj.is_alive() is True:
             break
 
         if time.time() > timeout:
-            error_message = (
-                f'The thread ({program_function_name}) timeout has reached its threshold of 1 minute.\n\n' +
-                (('-' * 150) + '\n') + (('-' * 65) + 'Additional Information' + ('-' * 63) + '\n') + (('-' * 150) + '\n') +
-                'Suggested Resolution:\n'
-                '  - Manual intervention is required for this thread to start.\n\n'
-                f'Originating error on line {traceback.extract_stack()[-1].lineno} in <{__name__}>\n' +
-                (('-' * 150) + '\n') * 2 
-            )   
-            raise ValueError(error_message)
+            error_args = {
+                'main_message': f'The thread ({program_function_name}) timeout has reached its threshold of 1 minute.',
+                'error_type': Exception,
+                'suggested_resolution': 'Manual intervention is required for this thread to start.',
+            }
+            error_formatter(error_args, __name__, get_line_number())
