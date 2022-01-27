b1f5c74a6cfe31a312fc3cd2a52a118d100e6600
IncognitoCoding <78840063+IncognitoCoding@users.noreply.github.com>
2022-01-26 23:22:36 -0600

    Update __init__.py

--- src/ictoolkit/__init__.py
+++ src/ictoolkit/__init__.py
@@ -2,7 +2,7 @@ __author__ = 'IncognitoCoding'
 __copyright__ = 'Copyright 2022, ictoolkit'
 __credits__ = ['IncognitoCoding']
 __license__ = 'GPL'
-__version__ = '3.0'
+__version__ = '3.1'
 __maintainer__ = 'IncognitoCoding'
 __status__ = 'Production'
 
